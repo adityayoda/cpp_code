82fa4fdd7f9aefa401427e54eb4589bc4385ac39
Aditya Singh <aditya1shahpur@gmail.com>
2022-01-11 15:48:38 +0530

    added table

--- table2.cpp
+++ table2.cpp
@@ -1,14 +1,21 @@
-#include<iostream>
+
+#include <iostream>
 
 using namespace std;
 
 int main()
-{
-  int j;
-  cin>>j;
-         for(int i=1; i<=10; i++)
-           {
-               cout<<j*i<<" ";
+{            
+    int j;
+    cin>>j;
+           for(int i=1; i<=10; i++)
+           { 
+             
+             cout<<j*i<<"  "<<\n;
+                 
            }
-    return 0;
+           
+           
+
+           
+           return 0;
 }
