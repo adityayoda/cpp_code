3f5fed8f59a3cf0588281fee5cbc0ec73e716c92
Aditya Singh <aditya1shahpur@gmail.com>
2022-01-24 18:47:15 +0530

    sample program of ap using for loop

---
+++ nthtermap.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+    int n, a=5, d=2, an=0, sn=0;
+    cin>>n;
+    for(int i=0; i<=n; i++)
+     {
+            an=a+(i-1)*d;
+     }
+         cout<<an<<" ";
+
+     
+
+
+
+    return 0;
+}
