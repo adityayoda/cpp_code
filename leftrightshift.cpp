1a8bb2bec246b58bd3a9f1762fb06c7bdb22c447
Aditya Singh <aditya1shahpur@gmail.com>
2022-01-25 10:16:53 +0530

    sample program for leftshift right operator

---
+++ leftrightshift.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin>>a;
+    int b;
+    cin>>b;
+    int c;
+    cin>>c;
+    int d;
+    cin>>d;
+    
+    cout<<(a>>1)<<endl;
+    cout<<(b>>2)<<endl;
+    cout<<(c<<1)<<endl;
+    cout<<(d<<1)<<endl;
+}
