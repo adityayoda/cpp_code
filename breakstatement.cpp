f7b2cad09a01e1a0f580da2e4b8b17079619ea12
Aditya Singh <aditya1shahpur@gmail.com>
2022-01-31 19:18:18 +0530

    added counting program using break statement

---
+++ breakstatement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    cout<<"print count from 1 to n"<<endl;
+        int i=1;
+        for(; ; ){
+        if
+        (i<=n){
+            cout<<i<<endl;
+            }
+            
+        else{
+              break; }
+            
+            i++;
+        
+    }
+}
