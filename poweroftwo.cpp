695e9edafc2b4225dc857c2dbbe1112ce3f0cd4c
Aditya Singh <aditya1shahpur@gmail.com>
2022-02-08 13:54:15 +0530

    added sample program for checking number is equal to power of two or not

---
+++ poweroftwo.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int ans=1;
+    for(int i=0;i<=30;i++)
+    {
+        if(ans==n)
+        {
+            cout<<true<<endl;
+        }
+        if(ans<INT_MAX/2)
+        ans=ans*2;
+     }
+     cout<<false<<endl;
+}
