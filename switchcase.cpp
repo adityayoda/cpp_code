ee4044460eb9ee45f9c9bd52628c66e5d30f7293
Aditya Singh <aditya1shahpur@gmail.com>
2022-02-09 17:02:53 +0530

    added sample program of deafault case using switch case

---
+++ switchcase.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch='a';
+    cout<<endl;
+    switch(ch){
+
+        case 1: 
+              cout<<"first"<<endl;
+              break;
+        case 2:
+               cout<<"charcter one"<<endl;
+               break;
+        default:
+                cout<<"it is a default case"<<endl;
+
+    }
+    cout<<endl;
+}
